8362109a841a05931262294a60bd1c612d7d3c91
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-06-07 18:42:37 +0300

    add connector empty tests

---
+++ test/connector.spec.js
@@ -0,0 +1,23 @@
+describe('./src/connector.js', function() {
+	'use strict';
+
+	describe('find document method', function () {
+
+		describe('when query on one field', function () {
+
+		});
+
+		describe('when query on several fields', function () {
+
+		});
+
+		describe('when use mongo operators', function () {
+
+		});
+	});
+
+	describe('update document method', function () {
+
+	});
+
+});
